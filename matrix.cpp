c0ca3e1faf603fe45343b18f5002ad84108f66b4
Ovchinnikov Anton <157784472+AntonITW@users.noreply.github.com>
2025-04-16 10:39:47 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -46,16 +46,35 @@ public:
         }
     }
 };
+template<typename T>
+void addition (T m1, T m2) {
+    int rows = m1.row; int cols = m1.col;
+    int arr[rows][cols];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) 
+            arr[i][j] = m1.data[i][j] + m2.data[i][j];
+        }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)  {
+                printf("%d    ", arr[i][j]);
+            }
+            printf("\n");
+    }
+}
 
 int main()
 {
-    int a, b;
-    printf("Enter the amount of rows: \n");
-    scanf("%d", &a);
-    printf("Enter the amount of columns: \n");
-    scanf("%d", &b);
-    Matrix <int> m(a,b);
-    m.print();
+    int rows, cols;
+    printf("Введите количество строк в матрице: \n");
+    scanf("%d", &rows);
+    printf("Введите количество столбцов в матрице: \n");
+    scanf("%d", &cols);
+    
+    Matrix <int> m1(rows, cols);
+    m1.print();
+    Matrix <int> m2(rows, cols);
+    m2.print();
+    addition(m1, m2);
+    
     return 0;
 }
-
